8f6c4284b37329c7724749de9c5fc0ed8103fadc
duykhanh <duykhanh1983@gmail.com>
2017-07-24 11:31:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Hello Nodejs");
+// app.js
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var express = require('express');
+var app = express();
+var http = require('http');
+var https = require('https');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var bodyParser = require("body-parser");
+
+var connection = new autobahn.Connection({
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/bower_components'));
+app.get('/', function (req, res, next) {
+    res.sendFile(__dirname + '/index.html');
+});
