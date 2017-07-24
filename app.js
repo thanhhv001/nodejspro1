4549e739f394f563f18c61aa947ddd47a862ec87
duykhanh <duykhanh1983@gmail.com>
2017-07-24 14:22:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -20,3 +20,21 @@ app.get('/', function (req, res, next) {
 });
 var port = 3000;
 server.listen(process.env.PORT || port);
+
+const ticker = require('cryptocurrency-ticker');
+
+ticker.availableExchanges().then((exchanges) => {
+	console.log(exchanges);
+});
+
+ticker.availablePairs('kraken').then((pairs) => {
+	console.log(pairs);
+}).catch((err) => {
+	console.error(err);
+});
+
+ticker.ticker('kraken', 'eth_jpy').then((ticker) => {
+	console.log(ticker);
+}).catch((err) => {
+	console.error(err);
+});
