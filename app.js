b0ac05c0f9c8a07df48eeb0010cbd927973e10e8
duykhanh <duykhanh1983@gmail.com>
2017-08-22 11:38:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,51 +9,84 @@ var http = require('http');
 var https = require('https');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
+var bittrex = require('node.bittrex.api');
+var autobahn = require('autobahn');
+var wsuri = "wss://api.poloniex.com";
 var bodyParser = require("body-parser");
+
+var connection = new autobahn.Connection({
+    url: wsuri,
+    realm: "realm1"
+});
 
 console.log("Folder:"+__dirname);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/bower_components'));
-app.get('/', function (req, res, next) {
-     request({
-    uri: 'https://poloniex.com/public?command=returnTicker',
-   
-    function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        console.log(body);
-        res.json(body);
-      } else {
-        res.json(error);
-      }
-    }
-  });
+
+app.get('/', function (req, res, next) {    
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/ticker',function(){
+	var url = 'https://poloniex.com/public?command=returnTicker';
+    var body = '';
+    https.get(url, function(response){
+        response.on('data', function(chunk){
+            body += chunk;
+        });
+
+        response.on('end', function(){
+            console.log("Got a response: ", body);
+            res.json(body);
+			console.log(res);
+        });
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+          res.json(e);
+    });
+})
+
 var port = 3000;
 server.listen(process.env.PORT || port);
+//Polo get ticker
+io.on('connection', function (client) {
+    console.log('Client connected...');
 
-// Import the module 
-var polo = require("poloniex-unofficial");
- 
-// Get access to the push API 
-var poloPush = new polo.PushWrapper();
-
-// Get price ticker updates 
-poloPush.ticker((err, response) => {
-    if (err) {
-        // Log error message 
-        console.log("An error occurred: " + err.msg);
- 
-        // Disconnect 
-        return true;
-    }
-    io.emit('message', response); 
-    // Check if this currency is in the watch list 
-    if (watchList.indexOf(response.currencyPair) > -1) {
-        // Log the currency pair and its last price 
-        console.log(response.currencyPair + ": " + response.last);
-    }
+    client.on('join', function (data) {
+        console.log(data);
+    });
+    
+    client.on('sendMessages', function (data) {
+        io.emit('broadcastMessages', data);
+    });
+	
+    client.on('tradeCoin',function(data){
+        io.emit('BTC_XMR',data);
+    });
 });
+
+connection.onopen = function (session) {
+    console.log("Websocket connection open");
+    function marketEvent(args, kwargs) {             
+        io.emit('tradeCoin', args);
+    }
+	
+    function tickerEvent(args, kwargs) {
+        io.emit('messages', args);
+    }
+
+    session.subscribe('BTC_XMR', marketEvent);
+    session.subscribe('ticker', tickerEvent);
+
+}
+
+connection.onclose = function (a,b) {
+    console.log(b);
+    console.log("Websocket connection closed");
+}
+
+connection.open();
+
+
