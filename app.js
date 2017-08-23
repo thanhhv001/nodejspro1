635db33471a20d3f6c879e97ff3a4ea88cdda0a7
duykhanh <duykhanh1983@gmail.com>
2017-08-23 11:24:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res, next) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/getPTicker',function(){
+app.post('/getPTicker',function(){
 	var url = 'https://poloniex.com/public?command=returnTicker';
     var body = '';
     https.get(url, function(response){
